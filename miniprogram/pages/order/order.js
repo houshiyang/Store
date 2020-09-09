829c1dcf03b31313e244629f073bd47139e799c2
houshiyang <bithoushiyang@163.com>
2020-09-09 17:53:50 +0800

    block reuse

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -1,11 +1,19 @@
 // pages/order/order.js
+const util = require("../../utils/util")
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo: null,
+  },
 
+  onTapLogin(event) {
+    this.setData({
+      userInfo: event.detail.userInfo
+    })
   },
 
   /**
@@ -26,7 +34,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    util.getUserInfo().then(userInfo => {
+      this.setData({
+        userInfo
+      })
+    })
   },
 
   /**
